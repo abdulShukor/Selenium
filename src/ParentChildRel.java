be3dfc000ae3355446fdb35506c5cc93b0bd316c
abdulShukor <ashukoor12@gmail.com>
2019-05-05 13:13:33 -0400

    Parent and Child relationship

---
+++ src/ParentChildRel.java
@@ -0,0 +1,27 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ParentChildRel {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.setProperty("webdriver.chrome.driver","C:\\work\\chromedriver.exe");
+		WebDriver driver =new ChromeDriver();
+		driver.get("http://www.qaclickacademy.com/interview.php");
+		driver.findElement(By.xpath("//li[text()=' Selenium ']")).click();
+		
+		// traverse through the parent node to the child if the child node is static 
+		// //tagname[@attribute='ad']/div/div/[2 mean second child]/input
+		// it the it dynamic then use sibling or parent relashionship 
+		
+
+		driver.findElement(By.xpath("//ul[@class='responsive-tabs__list']/li[1]/following-sibling::li[2]")).click();
+
+		System.out.println(driver.findElement(By.xpath(".//*[@id='tablist1-tab2']/parent::ul")).getAttribute("role"));
+		//
+
+	}
+
+}
--- src/SeleniumBasics.java
+++ src/SeleniumBasics.java
@@ -5,23 +5,24 @@ public class SeleniumBasics {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		//need download. you need .exe file for any browser you are using 
+
+		// need download. you need .exe file for any browser you are using
 		// for chrome is chrome.driver.chrome
 		// for firefox webdriver.gecko.driver
-		//for ie webdriver.ie.webdriver
-		
-		//you must have these two steps
+		// for ie webdriver.ie.webdriver
+
+		// you must have these two steps
 		System.setProperty("webdriver.chrome.driver", "D:\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
-		
+
 		driver.get("https://www.google.com/");
-		//printing to the console
-		//System.out.println(driver.getTitle());// validate the title
-		//System.out.println(driver.getCurrentUrl());// validating the correct url
-		//System.out.println(driver.getPageSource());// print the source code of the page 
+		// printing to the console
+		// System.out.println(driver.getTitle());// validate the title
+		// System.out.println(driver.getCurrentUrl());// validating the correct url
+		// System.out.println(driver.getPageSource());// print the source code of the
+		// page
 		driver.get("http://yahoo.com");
-		driver.navigate().to("http://centennailcollege.ca");// navigate from the current page to the target page 
+		driver.navigate().to("http://centennailcollege.ca");// navigate from the current page to the target page
 		driver.navigate().forward();
 		driver.navigate().back();
 //		driver.close(); // it close the current browser 
@@ -30,5 +31,3 @@ public class SeleniumBasics {
 	}
 
 }
-
-
