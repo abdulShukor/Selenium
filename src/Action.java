312ac9a079f23606bea7b9a1d2755a4000e118d2
abdulShukor <ashukoor12@gmail.com>
2019-05-10 20:05:50 -0400

    worked on calendar , action class, and scope

---
+++ src/Action.java
@@ -0,0 +1,18 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Action {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		// action class method is mouse  interaction, user gestures, keyboard action
+		// composite action 
+		//frame
+		System.setProperty("webdriver.chrome.driver", "D:\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		
+	}
+
+}
