36a221659992e07ad052be2a33988c2ac3a7d88c
abdulShukor <ashukoor12@gmail.com>
2019-05-19 22:48:17 -0400

    table, SSL, cookies

---
+++ src/FrameVlidation.java
@@ -0,0 +1,19 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FrameVlidation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.setProperty("webdriver.chrome.driver", "D:\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		
+		// isDisplayed() is use for item is visible or not 
+		// size() is use for item is present or not 
+		
+
+		
+	}
+
+}
