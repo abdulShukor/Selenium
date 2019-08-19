8fcca7d4785bef949da0b5c1aed15af809efeb15
abdulShukor <ashukoor12@gmail.com>
2019-08-19 02:11:05 -0400

    updated file

--- src/ParentChildRel.java
+++ src/ParentChildRel.java
@@ -12,16 +12,24 @@ public class ParentChildRel {
 		
 		driver.get("http://www.qaclickacademy.com/interview.php");
 		driver.findElement(By.xpath("//li[text()=' Selenium ']")).click();
+		// when using text make sure check for space before and after 
 		
 		// traverse through the parent node to the child if the child node is static 
 		// //tagname[@attribute='ad']/div/div/[2 mean second child]/input
 		// it the it dynamic then use sibling or parent relashionship 
+	// first is // and then / dev
 		
-
+		// 
 		driver.findElement(By.xpath("//ul[@class='responsive-tabs__list']/li[1]/following-sibling::li[2]")).click();
+		// parent tag is dynamic and one of its sibling is constant
 
 		System.out.println(driver.findElement(By.xpath(".//*[@id='tablist1-tab2']/parent::ul")).getAttribute("role"));
-		//
+		// using child xapth identify parent xpath 
+		// this is only possible in xpath you can traverse back but with css yo connat  traverse back.
+		// check first in tool like firepath
+		// with css you can do it without tag name 
+		// with #ID and with class .classname
+		// find unique if has the same field, find unique attribute 
 
 	}
 
